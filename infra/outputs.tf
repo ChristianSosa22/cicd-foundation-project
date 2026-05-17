548f1cddb69a097738bfa343b4cbe2c2ba7a6179
Gabriela López <anagabriela.lopez@galileo.edu>
2026-05-17 14:31:38 -0600

    expose compute and storage module output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,23 @@ output "bucket_arn" {
   description = "ARN of the S3 bucket. Use this when granting access to the bucket from IAM policies."
   value       = aws_s3_bucket.this.arn
 }
+
+output "compute_function_arn" {
+  description = "ARN of the Lambda function provisioned by the compute module."
+  value       = module.compute.function_arn
+}
+
+output "compute_function_name" {
+  description = "Name of the Lambda function provisioned by the compute module."
+  value       = module.compute.function_name
+}
+
+output "storage_bucket_name" {
+  description = "Name of the S3 bucket provisioned by the storage module."
+  value       = module.storage.bucket_name
+}
+
+output "storage_bucket_arn" {
+  description = "ARN of the S3 bucket provisioned by the storage module."
+  value       = module.storage.bucket_arn
+}
